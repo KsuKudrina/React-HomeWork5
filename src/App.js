ceb386b2aaebd3f1773a3f85af925449f94ef287
Ksu <ksenia.barteva@gmail.com>
2024-09-28 13:45:19 +0700

    React HomeWork 5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import './App.scss';
+import { store } from './components/store/store';
+import { Header } from './components/Page/Header';
+import Profile from './components/Page/Profile';
+import { Footer } from './components/Page/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div style = {{height:"100vh", display: "flex", flexDirection: "column",
+      justifyContent: 'space-between',
+      alignItems: 'stretch'}} className="App">
+        <Header />
+        <Profile />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
